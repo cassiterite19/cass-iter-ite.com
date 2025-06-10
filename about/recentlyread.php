dbc31b83225b97704cab14a47251043fde0875eb
cassiterite19 <alexandralilydavis@gmail.com>
2025-06-09 20:01:22 -0400

    added some to my recently read

--- about/recentlyread.php
+++ about/recentlyread.php
@@ -10,6 +10,8 @@
                     <div>
                         <h2>links</h2>
                         <ul>
+                            <li><a href="https://epochemagazine.org/41/berserk-metaphysics-on-the-idea-of-evil/">Berserk Metaphysics: On the Idea of Evil</a> by Antonio Wolf (read: 06/09/25)</li>
+                            <li><a href="https://techcrunch.com/2025/06/05/bonfires-new-software-lets-users-build-their-own-social-communities-free-from-platform-control/">Bonfire’s new software lets users build their own social communities, free from platform control</a> by Sarah Perez (read: 06/09/25)</li>
                             <li><a href="https://www.chefsresource.com/faq/does-the-body-metabolize-alcohol-after-death/">Does the body metabolize alcohol after death?</a> by Rachel Bannarasee (read: 05/20/25)</li>
                             <li><a href="https://leilukin.com/blog/posts/2025-05-19-my-vps-arc-began/">My Virtual Private Server Arc Has Officially Begun</a> by Leilukin (read: 05/19/25)</li>
                         </ul>
