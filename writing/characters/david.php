73d7f2d9f824adf24a9947d35001dcd51ac08e56
cassiterite19 <alexandralilydavis@gmail.com>
2025-06-10 18:38:32 -0400

    added David pictures

--- writing/characters/david.php
+++ writing/characters/david.php
@@ -6,7 +6,9 @@
             </header>
             <section>
                 <article>
-                    <img src="https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ehmkruqiniunazquhf3njjun/bafkreidquwvipchk2qqgdudeo4pv4ey577u5qyqflgy7ocamizeuzguwha@jpeg" alt="a sketchbook drawing of a vampire with long dark hair." />
+                    <img src="david_01.jpg" alt="a sketchbook drawing of a vampire with long dark hair." />
+                    <br />
+                    <img src="david_02.jpg" alt="a drawing of a vampire with long dark hair in a fishnet shirt." />
                     <p>David is a vampire from present day Germany, he was born in 9CE. He is angsty as all vampires tend to be but he loves learning and has an unbreakable sense of wonder at the universe. (He loves the stars.) He listens to the band HIM because he is incapable of feeling shame. He is a talented artist and makes money doing art commissions for wealthy furries and other internet patrons.</p>
                     <h3>stories:</h3>
                     <ul>
