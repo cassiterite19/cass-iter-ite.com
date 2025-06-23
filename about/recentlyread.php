6e02d707b202ac8bd4916d934031da9618db43da
cassiterite19 <alexandralilydavis@gmail.com>
2025-06-23 14:20:12 -0400

    0623: added to recently read

--- about/recentlyread.php
+++ about/recentlyread.php
@@ -10,6 +10,7 @@
                     <div>
                         <h2>links</h2>
                         <ul>
+                            <li><a href="https://rentry.co/what">what</a> by rentry (read: 06/23/25)</li>
                             <li><a href="https://blog.avas.space/future-online-creators/">the future of online creators</a> by ava (read: 06/22/25)</li>
                             <li><a href="https://otherkin.wiki/wiki/Extranthrope">The Otherkin Wiki page on Extranthrope</a> (read: 06/20/25)</li>
                             <li><a href="https://karlkerschl.com/how-i-built-my-own-patreon-alternative/">How I Built My Own Patreon Alternative</a> by Karl Kerschl (read: 06/16/25)</li>
--- index.php
+++ index.php
@@ -22,8 +22,8 @@
                     <div>
                         <h2>latest posts:</h2>
                         <ul>
-                            <li><a href="/writing/stories/03.php">why David hates the Romans</a> (posted 06/23/25)</li>
-                            <li><a href="/about/recentlyread.php">recently read</a> (last updated: 06/22/25)</li>
+                            <li><a href="/about/recentlyread.php">recently read</a> (last updated: 06/23/25)</li>
+                            <li><a href="/writing/stories/03.php">why David hates the Romans</a> (posted: 06/23/25)</li>
                             <li><a href="/writing/characters.php">characters</a> (last updated: 05/11/25)</li>
                             <li><a href="/websites/start.php">start page</a> (last updated: 02/13/25)</li>
                             <li><a href="/websites/head.php">you should put this in your &lt;head&gt; tag</a> (last updated: 10/05/24)</li>
