5e93af28e6c0d1f62e1bd94ef326edb4c0658db9
cassiterite19 <alexandralilydavis@gmail.com>
2025-07-02 22:27:36 -0400

    0702: trying to add a midi file to my website like it's 2003

--- header.php
+++ header.php
@@ -9,6 +9,11 @@
         <link href="/style.css" rel="stylesheet" type="text/css" media="all">
     </head>
 	<body>
+        <midi-player src="/music/LastSurprise.mid" sound-font>
+        </midi-player>
+
+        <!-- The following needs to be inserted somewhere on the page for the player(s) to work. -->
+        <script src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.22.1/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.4.0"></script>
 		<header>
 			<h1><a href="/">cassiterite</a></h1>
         </header>
