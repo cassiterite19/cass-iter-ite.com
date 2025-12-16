6a327bee8740fab121ff1e0cbe4ea682e1867d29
cassiterite19 <alexandralilydavis@gmail.com>
2025-12-16 01:32:50 -0500

    1216: added contact form

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/sidebar.php';?>
+            <header>
+                <h1>contact</h1>
+                <p>if you want to get in touch with me, use this handy contact form!</p>
+                <p>(you can also <a href="https://cassiterite.atabook.org/">leave a message in my guestbook</a> if you wanna.)</p>
+            </header>
+            <section>
+                <article>
+                    <div>
+                        <script data-letterbirduser="cassiterite" src="https://letterbird.co/embed/v1.js"></script>
+                    </div>
+                </article>
+            </section>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php';?>
--- sidebar.php
+++ sidebar.php
@@ -3,19 +3,12 @@
             <nav>
 				<ul>
                     <li><h3><a href="/about/">about</a></h3></li>
-                    <li><a href="/about/faves.php">faves</a></li>
-                    <li><a href="/about/recentlyread.php">recently read</a></li>
 					<li><h3><a href="/art/">art</a></h3></li>
-                    <li><a href="/art/myartjourney.php">my art journey</a></li>
 					<li><h3><a href="/music/">music</a></h3></li>
 					<li><h3><a href="/websites/">websites</a></h3></li>
-                    <li><a href="/websites/head.php">your &lt;head&gt; tag</a></li>
-                    <li><a href="/websites/start.php">start page</a></li>
 					<li><h3><a href="/writing/">writing</a></h3></li>
-                    <li><a href="/writing/characters/">characters</a></li>
-                    <li><a href="/writing/stories/">stories</a></li>
 					<li><h3><a href="/vgames/">video games</a></h3></li>
-                    <li><a href="/vgames/homm3.php">homm3</a></li>
+                    <li><h3><a href="/contact.php">contact</a></h3></li>
                     <li><h3><a href="https://cassiterite.atabook.org/">guestbook</a></h3></li>
                     <li><h3><a href="https://cassiterite.flarum.cloud/">forum</a></h3></li>
                     <!-- <li><h3><a href="https://blog.cass-iter-ite.com">book blog</a></h3></li> -->
