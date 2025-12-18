69253c7dd15598313719950c1be38ef3024b7d6b
cassiterite19 <alexandralilydavis@gmail.com>
2025-12-18 01:06:48 -0500

    1218: edited and added to sidebar links (reacently read page)

--- sidebar.php
+++ sidebar.php
@@ -9,11 +9,12 @@
 					<li><h3><a href="/writing/">writing</a></h3></li>
 					<li><h3><a href="/vgames/">video games</a></h3></li>
                     <li><h3><a href="/contact.php">contact</a></h3></li>
-                    <li><h3><a href="https://cassiterite.atabook.org/">guestbook</a></h3></li>
-                    <li><h3><a href="https://cassiterite.flarum.cloud/">forum</a></h3></li>
-                    <!-- <li><h3><a href="https://blog.cass-iter-ite.com">book blog</a></h3></li> -->
-                    <li><h3><a href="https://cassiterite19.github.io/">website log</a></h3></li>
-                    <li><h3><a href="https://cassiterite.leaflet.pub">weed thoughts</a></h3></li>
+                    <li><a href="https://read.cass-iter-ite.com/index.php/">Recently Read</a></li>
+                    <li><a href="https://cassiterite.atabook.org/">guestbook</a></li>
+                    <li><a href="https://cassiterite.flarum.cloud/">forum</a></li>
+                    <li><a href="https://blog.cass-iter-ite.com">book blog</a></li>
+                    <li><a href="https://cassiterite19.github.io/">website log</a></li>
+                    <!-- <li><a href="https://cassiterite.leaflet.pub">weed thoughts</a></li> -->
 				</ul>
 			</nav>
         </aside>
